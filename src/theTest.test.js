34fb11b081fb566a5547c209e164a1cbe74a639f
Mark Pearl <mark.pearl@myob.com>
2017-02-18 10:58:46 +1300

    Get babel working with mocha

---
+++ src/theTest.test.js
@@ -0,0 +1,8 @@
+import {assert as assert} from 'chai'
+import Mocha from 'mocha'
+
+describe("Testing the assert", function() {
+    it("is true", function() {
+       assert.equal('1','1'); 
+    });
+});
